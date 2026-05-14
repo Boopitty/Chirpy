6249e8dcc25008f84be4535deb4da628157a86f0
Emil Auguste <emilrauguste@gmail.com>
2026-05-14 03:55:01 -0400

    Refactor server setup and add index.html file

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	serverMux := http.NewServeMux()
+	// Create a server object with a mutex
+	mux := http.NewServeMux()
+	port := "8080"
 	server := http.Server{
-		Addr:    ":8080",
-		Handler: serverMux,
+		Addr:    ":" + port, // this means the site will be run on a local server
+		Handler: mux,
 	}
-	err := http.ListenAndServe(server.Addr, server.Handler)
-	if err != nil {
-		fmt.Printf("Problem running server: %s", err)
-	}
+
+	// serve the index.html file on the site on the home page
+	var path http.Dir = "."
+	filepathRoot := http.FileServer(path)
+	mux.Handle("/", filepathRoot)
+
+	// Run ListenAndServe to run the site.
+	// The code is blocked from this point until the server is closed or craches.
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
